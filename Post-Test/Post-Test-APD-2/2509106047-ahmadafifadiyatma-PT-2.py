7f531527d4d272cd2d3c557a3ebd8929857b9cef
Ahmad-Afif-4707 <ahmadafifadiyatma@gmail.com>
2025-09-23 09:45:48 +0800

    posttest 2

---
+++ Post-Test/Post-Test-APD-2/2509106047-ahmadafifadiyatma-PT-2.py
@@ -0,0 +1,32 @@
+# Suhu Celcius
+suhu = [27, 33, 46, 55, 67, 92]
+
+# Konversi suhu
+# suhu_1 dan suhu_2 > Fahrenheit
+fahrenheit = [suhu[0] * 9/5 + 32, suhu[1] * 9/5 + 32]
+
+# suhu_3 dan suhu_4 > Kelvin
+kelvin = [suhu[2] + 273.15, suhu[3] + 273.15]
+
+# suhu_5 dan suhu_6 >  Reamur
+reamur = [suhu[4] * 4/5, suhu[5] * 4/5]
+
+# Jumlahkan suhu
+jumlah = (fahrenheit[0] + fahrenheit[1] + kelvin[0] + kelvin[1] + reamur[0] + reamur[1])
+
+# Rata-rata
+ratarata = jumlah / 6
+
+# Nim
+NIM = 47
+
+bool = NIM < ratarata
+
+print("Suhu Celcius: ", suhu)
+print("Fahrenheit: ", fahrenheit)
+print("Kelvin: " , kelvin)
+print("Reamur: ", reamur)
+print("Jumlah semua suhu", jumlah)
+print("Rata-rata suhu", ratarata)
+print("NIM", NIM)
+print("Apakah NIM < ratarata", bool)
